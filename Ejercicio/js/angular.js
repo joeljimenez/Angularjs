0b795defe7a9f1a2b30e00bf3b8361fbd4ee98d8
joel jimenez <tonyjoe1110@hotmail.com>
2019-10-30 04:17:22 -0500

    pryectos angular js

---
+++ Ejercicio/js/angular.js
@@ -0,0 +1,19 @@
+(function(){
+    'use strict';
+
+    angular
+        .module('Module')
+        .controller('ControllerCtrl', ControllerCtrl)
+
+    /** @ngInject */
+    function ControllerCtrl(Dependencies){
+        var vm = this;
+        
+        init();
+
+        function init(){
+        }
+
+    }
+
+}());
